61224fa3e7bfac2f1cde992e2609b76e94b4ee6a
Nikhil-Mishra12 <nm280477@gmail.com>
2025-08-28 05:54:27 +0530

    completed city repository

---
+++ src/repositry/city-repository.js
@@ -0,0 +1,53 @@
+const { where } = require('sequelize');
+const {City}=require('../models/index');
+
+class CityRepository {
+    async createcity({name}){
+try {
+        const city=await City.create({
+            name
+        });
+        return city;
+    } catch (error) {
+        console.log("something went wrong in repository layer");
+        throw(error)
+    }
+    }
+    
+async deleCity(cityid){
+    try {
+        await City.destroy({
+            where:{
+                id:cityid
+            }
+        });
+        return true;
+    } catch (error) {
+        console.log("something went wrong in repository layer");
+        throw(error)
+    }
+}
+
+async updateCity(cityid,data){
+    try {
+        const city=await City.update(data,{
+            where:cityid
+        })
+    } catch (error) {
+         console.log("something went wrong in repository layer");
+        throw(error)
+    }
+}
+
+
+async getCity(cityid){
+   try {
+     const city=await City.findByPk(cityid);
+    return city;
+   } catch (error) {
+    console.log("something went wrong in repository layer");
+    throw(error)
+   }
+}
+
+}
